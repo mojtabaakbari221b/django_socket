e6b4882d9712f7d0c07abefab6c0850612a3850e
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2021-10-24 17:24:39 +0330

    logic of commincate users in group chat

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,12 +1,45 @@
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+import json
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        print(self.channels_name)
+        self.user_name = self.scope['url_route']['kwargs']['user_name']
+        self.group_name = self.scope['url_route']['kwargs']['group_name']
+        self.room_group_name = f"group_{self.group_name}"
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
 
     def receive(self, text_data):
-        pass
+        if text_data :
+            user_data = json.loads(text_data)
+            message = user_data["message"]            
+            sender = user_data["sender"]
+            async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_group_message',
+                'message': message,
+                'sender' : sender,
+            }
+            )
+    
+    def chat_group_message(self , event):
+        if self.user_name != event['sender'] :
+            message = event['message']
+            sender = event['sender']
+
+            # Send message to WebSocket
+            self.send(text_data=json.dumps({
+                'message': message,
+                'sender' : sender,
+            }))
--- chat/routing.py
+++ chat/routing.py
@@ -2,5 +2,5 @@ from . import consumers
 from django.urls import path
 
 websocket_urlpatterns = [
-    path('ws/chat/<str:username>/', consumers.ChatConsumer.as_asgi()),
+    path('ws/groupchat/<str:user_name>/to/<str:group_name>/', consumers.ChatConsumer.as_asgi()),
 ]

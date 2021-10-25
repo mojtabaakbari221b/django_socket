742fbe9790a5083eea67ccc1f254e3e1e2246185
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2021-10-25 16:45:57 +0330

    convert async cosumer to async in caht app

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,45 +1,52 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 from asgiref.sync import async_to_sync
 import json
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.user_name = self.scope['url_route']['kwargs']['user_name']
         self.group_name = self.scope['url_route']['kwargs']['group_name']
         self.room_group_name = f"group_{self.group_name}"
-        async_to_sync(self.channel_layer.group_add)(
+
+        # it's for sync case
+        # async_to_sync(self.channel_layer.group_add)(
+        #     self.room_group_name,
+        #     self.channel_name
+        # )
+
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         if text_data :
             user_data = json.loads(text_data)
             message = user_data["message"]            
             sender = user_data["sender"]
-            async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'chat_group_message',
-                'message': message,
-                'sender' : sender,
-            }
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'chat_group_message',
+                    'message': message,
+                    'sender' : sender,
+                }
             )
     
-    def chat_group_message(self , event):
+    async def chat_group_message(self , event):
         if self.user_name != event['sender'] :
             message = event['message']
             sender = event['sender']
 
             # Send message to WebSocket
-            self.send(text_data=json.dumps({
+            await self.send(text_data=json.dumps({
                 'message': message,
                 'sender' : sender,
             }))
